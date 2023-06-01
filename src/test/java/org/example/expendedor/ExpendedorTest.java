c9330d01b3389c179bd38c5dc096df7e3944fa62
ngonz <nicgonzalez2020@udec.cl>
2023-05-31 22:45:43 -0400

    Test

--- src/test/java/org/example/expendedor/ExpendedorTest.java
+++ src/test/java/org/example/expendedor/ExpendedorTest.java
@@ -7,9 +7,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpendedorTest {
-
+    private ExpendedorTest expendedortest;
     @BeforeEach
     void setUp() {
+        expendedortest = new expendedortest(3);
     }
 
     @AfterEach
